394856b65fd5d38c4614d8c6a92eea53b88c1b41
dipnlik <dipnlik@gmail.com>
2013-05-27 14:23:34 -0300

    username/password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_digest, :username
+  attr_accessible :username, :password, :password_confirmation, :password_digest
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    User.create(username: 'user', password: 'pass', password_confirmation: 'pass')
+  end
+  
+  it "username should be present and unique" do
+    User.new(username: '').should have(1).errors_on(:username)
+    User.new(username: 'user').should have(1).errors_on(:username)
+    User.new(username: 'uSeR').should have(1).errors_on(:username)
+    User.new(username: 'another').should have(:no).errors_on(:username)
+  end
+  
+  it "password should be present and confirmed" do
+    User.new(password: '', password_confirmation: '').should have(1).errors_on(:password)
+    User.new(password: '123', password_confirmation: '').should have(1).errors_on(:password)
+    User.new(password: '123', password_confirmation: '456').should have(1).errors_on(:password)
+    User.new(password: '123', password_confirmation: '123').should have(:no).errors_on(:password)
+  end
 end
