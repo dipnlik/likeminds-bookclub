369c2548dd3dd8c2294b5d2b8551f5b766aa4f95
dipnlik <dipnlik@gmail.com>
2013-05-27 17:25:53 -0300

    login form, sign up link

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe "sessions/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should show alert, if any" do
+    flash[:alert] = 'Login error'
+    render
+    assert_select 'p#alert', 'Login error'
+  end
+  
+  it "should have a login form" do
+    render
+    assert_select "form[action=?][method=?]", login_path, "post" do
+      assert_select "input#username[name=username]"
+      assert_select "input#password[type=password][name=password]"
+      assert_select "input[type=submit]"
+    end
+  end
+  
+  it "should have a link to create an account" do
+    render
+    assert_select "a[href=/users/new]"
+  end
 end
