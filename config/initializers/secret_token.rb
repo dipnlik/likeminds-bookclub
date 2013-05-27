94b2a4439ceec469fd26fb8977e4dc45b2bc24ab
dipnlik <dipnlik@gmail.com>
2013-05-27 13:32:18 -0300

    rails new bookclub -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookclub::Application.config.secret_token = '675bcef8dd9ab6eb6a281ea5a9d6e9b1ac34cf10c79239ee0a4445f3ebd6ef32a9ecb0246770178d48acf4f779ea8d828017741d41d8bdb83dab5434323e4c9d'
