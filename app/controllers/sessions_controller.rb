6343ba2f84aa09621b1ed35185d14e268d2a39d3
dipnlik <dipnlik@gmail.com>
2013-05-27 16:39:44 -0300

    SessionsController#create and #destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_username params[:username]
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to login_path, alert: 'Username/password mismatch!'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,17 +9,31 @@ describe SessionsController do
     end
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+  describe "POST 'create'" do
+    before(:each) do
+      @user = User.create!(username: 'user', password: 'pass', password_confirmation: 'pass')
+    end
+    
+    it "with correct credentials, should log user in" do
+      post :create, username: 'user', password: 'pass'
+      session[:user_id].should eq(@user.id)
+      response.should redirect_to user_url(@user)
+    end
+    
+    it "with wrong credentials, should not log in and warn the user" do
+      post :create, username: 'user', password: 'wrong'
+      session[:user_id].should be_blank
+      flash[:alert].should_not be_blank
+      response.should redirect_to login_url
     end
   end
 
   describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
+    it "should log out the user and redirect to the login page" do
+      session[:user_id] = -1
+      delete :destroy
+      session[:user_id].should be_nil
+      response.should redirect_to login_url
     end
   end
 
