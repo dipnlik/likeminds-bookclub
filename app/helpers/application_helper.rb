3bcab8c2142ae151018c6dc28d427ace1ba69b5f
dipnlik <dipnlik@gmail.com>
2013-05-28 19:34:12 -0300

    added website title with login info, logout link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def login_header
+    return '' if session[:user_id].nil?
+    
+    user = User.find(session[:user_id])
+    username_link = link_to(user.username, user)
+    logout_link = link_to('not you?', logout_path)
+    
+    content_tag 'small' do
+      raw "(welcome, #{username_link}. #{logout_link})"
+    end
+  end
 end
