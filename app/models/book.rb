4623f6ba61203449bf3858725d7077d4065f74bb
dipnlik <dipnlik@gmail.com>
2013-05-28 12:39:40 -0300

    book title/isbn simple validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
   attr_accessible :isbn, :title
+  validates :title, presence: true
+  validates :isbn, uniqueness: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Book do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    Book.create(title: 'A Book', isbn: '0123456789')
+  end
+  
+  it "should have a name" do
+    Book.create(title: '').should have(1).errors_on(:title)
+    Book.create(title: 'A Book').should have(:no).errors_on(:title)
+  end
+  
+  it "should have an unique and valid isbn" do
+    Book.create(isbn: '0123456789').should have(1).errors_on(:isbn)
+    Book.create(isbn: '0123456788').should have(:no).errors_on(:isbn)
+  end
 end
