da2c44c300acd9b8ad9266432fff45de05c278b6
dipnlik <dipnlik@gmail.com>
2013-05-28 13:31:12 -0300

    rating validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,7 @@
 class Rating < ActiveRecord::Base
   attr_accessible :book_id, :user_id, :value
+  
+  validates :book_id, presence: true, uniqueness: {scope: :user_id}
+  validates :user_id, presence: true
+  validates :value, inclusion: (0..5)
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Rating do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    Rating.create!(book_id: 1, user_id: 1, value: 5)
+  end
+  
+  it "should have unique book_id and user_id" do
+    Rating.new(book_id: 1, user_id: 1).should have(1).error_on(:book_id)
+    Rating.new(book_id: nil, user_id: 1).should have(1).error_on(:book_id)
+    Rating.new(book_id: 1, user_id: nil).should have(1).error_on(:user_id)
+  end
+  
+  it "value should be between 0 and 5" do
+    Rating.new(value: 5.1).should have(1).error_on(:value)
+    Rating.new(value: -1).should have(1).error_on(:value)
+    Rating.new.should have(:no).error_on(:value)
+    Rating.new.value.should eq(0)
+  end
 end
