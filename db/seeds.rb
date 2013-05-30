720c410d7ca39bb7b206673c9c29eaecd9932a68
dipnlik <dipnlik@gmail.com>
2013-05-30 17:48:54 -0300

    added dummy seed data: 50 users, 1000 books, a few hundred ratings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(username: 'dipnlik', password: 'simple password', password_confirmation: 'simple password')
+
+# app should be able to scale to 50 users and 1000 books
+print "Creating users "
+(1..50).each do |i|
+  User.create(username: "user#{i}", password: 'dummy', password_confirmation: 'dummy')
+  print '.'
+end
+
+puts
+
+print "Creating books "
+(1..1000).each do |i|
+  Book.create(title: "Book ##{"%04d" % i}", isbn: ("%010d" % i))
+  print '.'
+end
+
+puts
+
+module ActiveRecord
+  class Base
+    def self.random
+      if (c = count) != 0
+        find(:first, offset: rand(c))
+      end
+    end
+  end
+end
+
+print "Creating random ratings "
+(1..200).each do |i|
+  book = Book.random
+  Kernel.rand(10).times do
+    Rating.create(book_id: book.id, user_id: User.random.id, value: Kernel.rand(6).to_f)
+    print '.'
+  end
+end
